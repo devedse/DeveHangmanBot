7322639ce95a1c98a0c94b01a2d856b877d699e9
Devedse <davydavidse@gmail.com>
2019-12-31 15:30:49 +0100

    Another fix

--- Scripts/set-version.py
+++ Scripts/set-version.py
@@ -9,14 +9,15 @@ files = [f for f in os.listdir('.') if os.path.isfile(f)]
 for f in files:
   print(f)
 
-version = os.getenv('TRAVIS_BUILD_ID', 0)
+buildId = os.getenv('TRAVIS_BUILD_ID', 0)
+version = "1.0.0.{}".format(buildId)
 
 print("Setting version: {}".format(version))
 
 def setVersion(filePath):
   with open (filePath, 'r' ) as f:
     content = f.read()
-    content_new = re.sub('(?<=<Version>).*(?=<\/Version>)', f'1.0.0.{version}', content, flags = re.M)
+    content_new = re.sub('(?<=<Version>).*(?=<\/Version>)', version, content, flags = re.M)
     print(content_new)
   with open (filePath, 'w') as f:
     f.write(content_new)
