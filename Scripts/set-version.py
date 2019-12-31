93c65a394ba030d563696801f2b08e436385f9dc
Devedse <davydavidse@gmail.com>
2019-12-31 16:34:45 +0100

    Cleanup

--- Scripts/set-version.py
+++ Scripts/set-version.py
@@ -4,20 +4,11 @@ import os
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 parentdir = os.path.abspath(os.path.join(dname, os.pardir))
-#os.chdir(parentdir)
-
-
 
 buildId = os.getenv('TRAVIS_BUILD_NUMBER', 0)
 version = "1.0.0.{}".format(buildId)
 
 print("Setting version: {}".format(version))
-
-def listFiles(path):
-  print("Listing files in: {}".format(path))
-  files = [f for f in os.listdir(path)]
-  for f in files:
-    print(f)
 
 def setVersion(fileName):
   filePath = os.path.join(parentdir, fileName)
@@ -27,10 +18,6 @@ def setVersion(fileName):
     print(content_new)
   with open (filePath, 'w') as f:
     f.write(content_new)
-
-listFiles('/home/travis/build/devedse/DeveHangmanBot/DeveHangmanBot')
-listFiles('/home/travis/build/devedse/DeveHangmanBot')
-listFiles('/home/travis/build/devedse')
 
 setVersion('DeveHangmanBot/DeveHangmanBot.csproj')
 setVersion('DeveHangmanBot.TelegramBot/DeveHangmanBot.TelegramBot.csproj')
