3c0c9ca6e20fceeff8226a8cc9cb7e7b7dd507cf
Devedse <davydavidse@gmail.com>
2020-01-17 10:53:26 +0100

    Wrote some if checks to make image code a bit safer

--- DeveHangmanBot/ImageStuff/ImageObtainer.cs
+++ DeveHangmanBot/ImageStuff/ImageObtainer.cs
@@ -22,62 +22,82 @@ namespace DeveHangmanBot.ImageStuff
 
         public async Task<byte[]> GetGoogleImageBytes(string word)
         {
-            try
+            if (!string.IsNullOrWhiteSpace(_botConfig.GoogleApiKey) && !string.IsNullOrWhiteSpace(_botConfig.GoogleCxToken))
             {
-                using (var httpClient = new HttpClient())
+                try
                 {
-                    var result = await httpClient.GetAsync($"https://www.googleapis.com/customsearch/v1?key={_botConfig.GoogleApiKey}&cx={_botConfig.GoogleCxToken}&q={word}&searchType=image");
-
-                    var stringified = await result.Content.ReadAsStringAsync();
-
-                    var parsed = JsonConvert.DeserializeObject<PocoGoogleCustomSearch>(stringified);
-
-                    var imgurl = parsed.items[0].link;
-
-                    if (!string.IsNullOrWhiteSpace(imgurl))
+                    using (var httpClient = new HttpClient())
                     {
-                        var obtainedImage = await httpClient.GetAsync(imgurl);
+                        var result = await httpClient.GetAsync($"https://www.googleapis.com/customsearch/v1?key={_botConfig.GoogleApiKey}&cx={_botConfig.GoogleCxToken}&q={word}&searchType=image");
 
-                        var byteStuff = await obtainedImage.Content.ReadAsByteArrayAsync();
+                        var stringified = await result.Content.ReadAsStringAsync();
 
-                        return byteStuff;
+                        var parsed = JsonConvert.DeserializeObject<PocoGoogleCustomSearch>(stringified);
+
+                        var imgurl = parsed.items[0].link;
+
+                        if (!string.IsNullOrWhiteSpace(imgurl))
+                        {
+                            var obtainedImage = await httpClient.GetAsync(imgurl);
+
+                            var byteStuff = await obtainedImage.Content.ReadAsByteArrayAsync();
+
+                            return byteStuff;
+                        }
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                _logger.WriteError(ex.ToString());
+                catch (Exception ex)
+                {
+                    _logger.WriteError(ex.ToString());
 
-                //_logger.WriteError($"Length of GoogleApiKey: {_botConfig.GoogleApiKey.Length}");
-                //_logger.WriteError($"Length of GoogleCxToken: {_botConfig.GoogleCxToken.Length}");
+                    //_logger.WriteError($"Length of GoogleApiKey: {_botConfig.GoogleApiKey.Length}");
+                    //_logger.WriteError($"Length of GoogleCxToken: {_botConfig.GoogleCxToken.Length}");
+                }
+            }
+            else
+            {
+                _logger.Write("Skipping obtaining images because GoogleApiKey or GoogleCxToken are empty", LogLevel.Warning);
             }
 
             return null;
         }
 
+        /// <summary>
+        /// You can create this here:
+        /// https://cse.google.com/cse/create/new
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
         public async Task<List<string>> GetGoogleImagesLinks(string word)
         {
-            try
+            if (!string.IsNullOrWhiteSpace(_botConfig.GoogleApiKey) && !string.IsNullOrWhiteSpace(_botConfig.GoogleCxToken))
             {
-                using (var httpClient = new HttpClient())
+                try
                 {
-                    var result = await httpClient.GetAsync($"https://www.googleapis.com/customsearch/v1?key={_botConfig.GoogleApiKey}&cx={_botConfig.GoogleCxToken}&q={word}&searchType=image");
+                    using (var httpClient = new HttpClient())
+                    {
+                        var result = await httpClient.GetAsync($"https://www.googleapis.com/customsearch/v1?key={_botConfig.GoogleApiKey}&cx={_botConfig.GoogleCxToken}&q={word}&searchType=image");
 
-                    var stringified = await result.Content.ReadAsStringAsync();
+                        var stringified = await result.Content.ReadAsStringAsync();
 
-                    var parsed = JsonConvert.DeserializeObject<PocoGoogleCustomSearch>(stringified);
+                        var parsed = JsonConvert.DeserializeObject<PocoGoogleCustomSearch>(stringified);
 
-                    var imgurls = parsed.items.Where(t => t.link.EndsWith(".png", StringComparison.OrdinalIgnoreCase) || t.link.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) || t.link.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase)).Select(t => t.link);
+                        var imgurls = parsed.items.Where(t => t.link.EndsWith(".png", StringComparison.OrdinalIgnoreCase) || t.link.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) || t.link.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase)).Select(t => t.link);
 
-                    return imgurls.ToList();
+                        return imgurls.ToList();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.WriteError(ex.ToString());
+
+                    //_logger.WriteError($"Length of GoogleApiKey: {_botConfig.GoogleApiKey.Length}");
+                    //_logger.WriteError($"Length of GoogleCxToken: {_botConfig.GoogleCxToken.Length}");
                 }
             }
-            catch (Exception ex)
+            else
             {
-                _logger.WriteError(ex.ToString());
-
-                //_logger.WriteError($"Length of GoogleApiKey: {_botConfig.GoogleApiKey.Length}");
-                //_logger.WriteError($"Length of GoogleCxToken: {_botConfig.GoogleCxToken.Length}");
+                _logger.Write("Skipping obtaining images because GoogleApiKey or GoogleCxToken are empty", LogLevel.Warning);
             }
 
             return new List<string>();
@@ -85,32 +105,39 @@ namespace DeveHangmanBot.ImageStuff
 
         public async Task<string> GetGiphyImageLink(string word)
         {
-            try
+            if (!string.IsNullOrWhiteSpace(_botConfig.GiphyApiKey))
             {
-                using (var httpClient = new HttpClient())
+                try
                 {
-                    var result = await httpClient.GetAsync($"https://api.giphy.com/v1/gifs/search?api_key={_botConfig.GiphyApiKey}&q={word}&limit=1&offset=0&rating=G&lang=en");
-
-                    var stringified = await result.Content.ReadAsStringAsync();
-
-                    var parsed = JsonConvert.DeserializeObject<PocoGiphySearch>(stringified);
-
-                    if (parsed.data.Any())
+                    using (var httpClient = new HttpClient())
                     {
-                        var imgurl = parsed.data[0].images.downsized_large.url;
+                        var result = await httpClient.GetAsync($"https://api.giphy.com/v1/gifs/search?api_key={_botConfig.GiphyApiKey}&q={word}&limit=1&offset=0&rating=G&lang=en");
 
-                        if (!string.IsNullOrWhiteSpace(imgurl))
+                        var stringified = await result.Content.ReadAsStringAsync();
+
+                        var parsed = JsonConvert.DeserializeObject<PocoGiphySearch>(stringified);
+
+                        if (parsed.data.Any())
                         {
-                            return imgurl;
+                            var imgurl = parsed.data[0].images.downsized_large.url;
+
+                            if (!string.IsNullOrWhiteSpace(imgurl))
+                            {
+                                return imgurl;
+                            }
                         }
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                _logger.WriteError(ex.ToString());
+                catch (Exception ex)
+                {
+                    _logger.WriteError(ex.ToString());
 
-                //_logger.WriteError($"Length of GiphyApiKey: {_botConfig.GiphyApiKey.Length}");
+                    //_logger.WriteError($"Length of GiphyApiKey: {_botConfig.GiphyApiKey.Length}");
+                }
+            }
+            else
+            {
+                _logger.Write("Skipping obtaining images because GoogleApiKey or GoogleCxToken are empty", LogLevel.Warning);
             }
 
             return null;
