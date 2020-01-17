75ab800194083294e3b7599133f7a75d630290f8
Devedse <davydavidse@gmail.com>
2020-01-17 03:19:07 +0100

    Added some debugging stuff

--- DeveHangmanBot.WebApp/Controllers/HomeController.cs
+++ DeveHangmanBot.WebApp/Controllers/HomeController.cs
@@ -1,23 +1,39 @@
 ﻿using DeveHangmanBot.WebApp.Logging;
 using DeveHangmanBot.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace DeveHangmanBot.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _config;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IConfiguration config, ILogger<HomeController> logger)
         {
+            _config = config;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var configSettings = _config.GetChildren();
+            var configSettingsListified = configSettings.Select(t => new SettingValueModel()
+            {
+                Key = t.Key,
+                Value = t.Key.Contains("auth", StringComparison.OrdinalIgnoreCase) ||
+                        t.Key.Contains("password", StringComparison.OrdinalIgnoreCase) ||
+                        t.Key.Contains("token", StringComparison.OrdinalIgnoreCase) ||
+                        t.Key.Contains("secret", StringComparison.OrdinalIgnoreCase) ||
+                        t.Key.Contains("key", StringComparison.OrdinalIgnoreCase)
+                        ? new string('*', t.Value.Length) : t.Value
+            }).ToList();
+            return View(configSettingsListified);
         }
 
         public IActionResult Logging()
---
+++ DeveHangmanBot.WebApp/Models/SettingValueModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DeveHangmanBot.WebApp.Models
+{
+    public class SettingValueModel
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
